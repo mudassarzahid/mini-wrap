4930f6190e3e994ea6924e58e087bb6fb95153be
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2020-08-26 17:43:08 +0200

    Logic

--- spotify.py
+++ spotify.py
@@ -1,32 +1,96 @@
-import requests
 import json
+import requests
+import plotly.express as px
+import dateutil.parser
+import plotly as py
+import plotly.graph_objs as go
+import ipywidgets as widgets
+from scipy import special
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+
+#plotly.tools.set_credentials_file(username='Mudi99', api_key='ttM0yUXO9HVbNqjKTqEn')
 
 
-def get_recent_tracks():
+def make_recently_played_request():
+  token = 'BQAwCWrsKhQCUrhXUHydd25XiQTNgfPQ4Gub1RjnHgtMX_qhsx2_lRvjD3BgAR-kyzwzfRuupT_svougsTllfBKEfqKvm4C0i3XfZ8W-TnM3QCA2JXffCvpc3TNTPxS3xelcTWIChJgNyN_v26jNPks'
 
-  headers = {
-      'Authorization': 'Bearer BQDbZrv0nQMhwUANUEXd2-WTNWkoGuLE2HBMM6Z-RE0O7fZHSOojz3-f9iAL2BIf1AGU9qDfC6u1ld6tfm7F68AFJFdIwn38ZQYp8Ap3KfmPvJT9KFDBnuwT-g3CvlQFJ5nS6tteOojyrs7gfw',
-  }
+  headers = {'Authorization': "Bearer {}".format(token)}
 
-  params = (
-      ('limit', '10'),
-  )
+  params = (('limit', '50'),)
 
   response = requests.get('https://api.spotify.com/v1/me/player/recently-played', headers=headers, params=params)
 
-  return json.dumps(extract_data(response.json()))
+  return response
 
 
-def extract_data(tracks):
+def get_all_data():
+  response = make_recently_played_request()
+  return response.json()
 
-  songs = []
+
+def get_recent_tracks():
+  response = make_recently_played_request()
+  return json.dumps(extract_songs_artists_images(response.json()))
+
+
+def get_dates():
+  response = make_recently_played_request()
+  return extract_date(response.json())
+
+
+def extract_songs_artists_images(tracks):
+  sorted_list = []
   for i in tracks['items']:
     song = i['track']['name']
+    image = i['track']['album']['images'][1]['url']
+
     artists = []
     for j in i['track']['artists']:
       artists.append(j['name'])
 
-    songs.append({"Song": song,
-                  "Artists": artists})
+    sorted_list.append({"song": song,
+                        "artists": artists,
+                        "image": image})
 
-  return songs
+  return sorted_list
+
+
+def extract_date(data):
+  sorted_list = []
+  for i in data['items']:
+    date = i['played_at']
+    ms_played = i['track']['duration_ms']
+    time_plus_ms = dateutil.parser.isoparse(date) + timedelta(milliseconds=ms_played)
+
+    sorted_list.append({'date': str(dateutil.parser.isoparse(date)),
+                        'ms_played': ms_played,
+                        'time_plus_ms': str(time_plus_ms)})
+
+  return sorted_list
+
+
+def show_graph():
+  sorted_list = get_dates()
+
+  fig = px.line(sorted_list, x='date', y=[3, 5, 6, 5, 6, 7, 8, 8, 8, 88, 8, 8, 8, 8, 4, 3, 3, 1, 3, 4, 5, 6, 7, 4])
+  fig.show()
+
+
+def dddddd():
+
+  data = get_all_data()
+  date = []
+  ms_played = []
+  for i in data['items']:
+    date.append(i['played_at'])
+    ms_played.append((i['track']['duration_ms']) / 60000)
+
+  smoothTrace = {'type': 'scatter', 'mode': 'lines',
+                 'x': date, 'y': ms_played, 'line': {'shape': 'spline'}}
+
+  py.offline.iplot([smoothTrace])
+
+
+dddddd()
