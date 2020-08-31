1642693ed453c3983aa5bfcbfc8db0e17db07534
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2020-08-31 22:02:22 +0200

    Add class

--- data_requests.py
+++ data_requests.py
@@ -1,45 +1,56 @@
 import requests
-from tokens import get_recently_token
-from tokens import get_top_token
+from tokens import get_token
 
 
-def make_recently_request():
-  token = get_recently_token()
+class Spotify():
 
-  headers = {'Authorization': "Bearer {}".format(token)}
+  TOKEN = 'BQDKsbbwBnLd2_Qz3yHMtIVNnP8bx-ZUPMF2ivLxbt1K44HVKpolfQmq86CX63LJ2TAFg5C5VHQFhD4vP24nABvL9JGt5_NNsN19XZ7u64NMSiB28vVTKUVQeVIocK1o6jifNv4zffJFpQDAdTvoK3FrL-eVlBmP5mY_vF-23cbX1K6f7G1ItfXBWMHRcPVabL24F6u9SpsCLzA0HKTBbedtS_yz6DAgVX8LxJpbUWXQRCa2B7mjLrmWWRrlJWYyL5-_WPIsRJk'
 
-  params = (('limit', '50'),)
+  API_URL = 'https://api.spotify.com/v1/me/'
 
-  response = requests.get('https://api.spotify.com/v1/me/player/recently-played', headers=headers, params=params)
+  def __init__(self):
+    self.token = get_token()
 
-  return response
+    self.headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer {}".format(self.token)
+    }
 
+    self.params = {'limit': '50'}
 
-def make_top_artists_request():
-  top()
+  def make_recently_request():
 
-  response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers, params=params)
+    response = requests.get(
+        self.API_URL + 'player/recently-played',
+        headers=self.headers,
+        params=self.params
+    )
 
-  return response
+    if response.status_code == 200:
+      return response.json()
+    return None
 
+  def top_songs(self):
+    response = requests.get(
+        self.API_URL + 'top/songs',
+        headers=self.headers,
+        params=self.params
+    )
 
-def make_top_songs_request():
-  top()
+    if response.status_code == 200:
+      return response.json()
+    return None
 
-  response = requests.get('https://api.spotify.com/v1/me/top/songs', headers=headers, params=params)
+    return response
 
-  return response
+  def top_artists(self):
+    response = requests.get(
+        self.API_URL + 'top/artists',
+        headers=self.headers,
+        params=self.params
+    )
 
-
-def top():
-  token = get_top_token()
-
-  headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': "Bearer {}".format(token)
-  }
-
-  params = (
-      ('limit', '50'),
-  )
+    if response.status_code == 200:
+      return response.json()
+    return None
