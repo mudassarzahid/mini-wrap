39c6bdbd82a817262012fc3cf095afee32961caa
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2020-11-15 00:05:10 +0100

    Update gitignore

--- webapp/src/Collage/Collage.jsx
+++ webapp/src/Collage/Collage.jsx
@@ -23,10 +23,14 @@ const Collage = props => {
                enableLightbox={false}
                enableImageSelection={false}
 <<<<<<< HEAD
+<<<<<<< HEAD
                onClickThumbnail={(index) => {window.open(props.images[index].url, '_blank')}}/>
 =======
                onClickThumbnail={(index) => {window.open(props.images[index].url, '_blank', 'noreferrer')}}/>
 >>>>>>> 7957c8b... Update backend
+=======
+               onClickThumbnail={(index) => {window.open(props.images[index].url, '_blank')}}/>
+>>>>>>> 222e31f... Update gitignore
 
       <div className="collage-subtext">
         <span className="website-name">https://wrapped.mudi.me/</span>
