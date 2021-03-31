4d7f9009ca0372b22319e877dfbdc5376b837842
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-03-31 11:22:14 +0200

    Use BrowserRouter

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import Login from "./Login";
 import reportWebVitals from './reportWebVitals';
-import {Router, Switch, Route} from "react-router-dom";
-import {createBrowserHistory} from 'history'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-const history = createBrowserHistory()
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
+    <Router forceRefresh={true}>
       <Switch>
 
         <Route exact path="/"
